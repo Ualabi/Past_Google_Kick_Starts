288caa6328f56e25661d2b90b736f7c444be2880
Ualabi <jhinojoza@outlook.com>
2020-05-07 17:03:59 -0500

    another commit

---
+++ 2016/Round B/1 Sherlock and Parentheses.py
@@ -0,0 +1,7 @@
+# Link: https://codingcompetitions.withgoogle.com/kickstart/round/0000000000201c0c/0000000000201ca5
+T = int(input())
+for i in range(T):
+    L, R = map(int,input().split())
+    m = min(L,R)
+    ans = m*(m+1)//2
+    print('Case #{}: {}'.format(i+1,ans))
