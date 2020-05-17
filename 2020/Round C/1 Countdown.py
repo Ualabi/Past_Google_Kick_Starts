947cccf6cf0d3fb6043f8a8eba68a433e78b174f
Ualabi <jhinojoza@outlook.com>
2020-05-17 09:08:26 -0500

    problems solved

---
+++ 2020/Round C/1 Countdown.py
@@ -0,0 +1,22 @@
+# Link: https://codingcompetitions.withgoogle.com/kickstart/round/000000000019ff43/00000000003380d2
+
+T = int(input())
+for t in range(T):
+    N, K = map(int,input().split())
+    arr = list(map(int,input().split()))
+    
+    ans = 0
+    val = K
+    for x in arr:
+        if x == K:
+            val = K-1
+        elif x == val:
+            if val == 1:
+                ans += 1
+                val = K
+            else:
+                val -= 1
+        elif val < K:
+            val = K
+            
+    print('Case #{}: {}'.format(t+1,ans))
---
+++ 2020/Round C/2 Stable Wall.py
@@ -0,0 +1,35 @@
+# Link: https://codingcompetitions.withgoogle.com/kickstart/round/000000000019ff43/00000000003379bb
+
+def dfs(letras,taken,word):
+    if len(letras) == len(taken):
+        return word
+    for x in letras:
+        if x not in taken:
+            if len(letras[x] - taken) == 0:
+                w = dfs(letras,taken | {x},word+x)
+                if w != -1:
+                    return w
+    return -1
+
+def solve(arr,R,C):
+    letras = {}
+    for r in range(R):
+        for c in range(C):
+            if r == R-1:
+                if arr[r][c] not in letras:
+                    letras[arr[r][c]] = set()
+            else:
+                if arr[r][c] not in letras:
+                    letras[arr[r][c]] = set() 
+                if arr[r+1][c] != arr[r][c]:
+                    letras[arr[r][c]].add(arr[r+1][c])
+    return dfs(letras,set(),'')
+
+T = int(input())
+for t in range(T):
+    R, C = map(int,input().split())
+    arr = []
+    for x in range(R):
+        aux = list(input())
+        arr.append(aux)
+    print('Case #{}: {}'.format(t+1,solve(arr,R,C)))
