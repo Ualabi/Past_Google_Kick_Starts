179f7c88e884e5335d79d2bdc1cbfbdd219c16c8
Ualabi <jhinojoza@outlook.com>
2020-06-12 22:44:43 -0500

    another problem

---
+++ 2017/Round E/1 Trapezoid Counting [Pypy2].py
@@ -0,0 +1,56 @@
+# Link: https://codingcompetitions.withgoogle.com/kickstart/round/0000000000201bfe/0000000000201d24
+# Use PyPy2 to run it, it does not pass the TLE with Python 3/2
+
+T = int(input())
+for t in range(T):
+    N = int(input())
+    A = list(map(int,raw_input().split()))
+    
+    freq = {}
+    for a in A:
+        freq[a] = freq.get(a,0) + 1
+    
+    val = sorted(freq)
+    lim = len(val)
+        
+    past = 0    
+    suma = {}
+    for f in val:
+        past += freq[f]
+        suma[f] = past
+        
+    ans = 0
+    for a in range(lim):
+        f = freq[val[a]]
+        if 2 <= f:
+            if 3 <= f:
+                l, r = a, lim-1
+                while l <= r:
+                    k = (l+r)//2
+                    if val[k] < 3*val[a]:
+                        l = k+1
+                    else:
+                        r = k-1
+                x = suma[val[r]] - f
+                p = (f*(f-1)*(f-2))//6
+                ans += x*p
+            
+            x = 0
+            for b in range(lim-1):
+                if b != a:
+                    y = freq[val[b]]
+                    l, r = b, lim-1
+                    while l <= r:
+                        k = (l+r)//2
+                        if val[k] < val[b] + 2*val[a]:
+                            l = k+1
+                        else:
+                            r = k-1
+                    z = suma[val[r]] - suma[val[b]]
+                    o = -f if b < a and a <= r else 0
+                    z += o
+                    x += y*z
+            p = (f*(f-1))//2
+            ans += x*p
+
+    print('Case #{}: {}'.format(t+1,ans))
--- probes.py
+++ probes.py
@@ -1,18 +1,119 @@
 T = int(input())
 for t in range(T):
-    W = input()
-    ln = len(W)
-    if ln%2 == 1:
-        print('Case #{}: AMBIGUOUS'.format(t+1))
-        continue
+    N = int(input())
+    A = list(map(int,input().split()))
+    
+    freq = {}
+    for a in A:
+        freq[a] = freq.get(a,0) + 1
+    
+    val = sorted(freq)
+    lim = len(val)
+        
+    past = 0    
+    suma = {}
+    for f in val:
+        past += freq[f]
+        suma[f] = past
+        
+    ans = 0
+    for a in range(lim):
+        f = freq[val[a]]
+        if 3 <= f:
+            for b in range(a,lim):
+                if 3*val[a] <= val[b]:
+                    break
+            if a < b:
+                x = suma[b] - f
+            elif b == a and a > 0:
+                x = suma[val[a-1]]
+            else:
+                x = 0
+            p = (f*(f-1)*(f-2))//6
+            ans += x*p
+        
+        if 2 <= f:
+            x = 0
+            for b in range(lim):
+                if b != a:
+                    y = freq[val[b]]
+                    z = 0 
+                    for c in range(b+1,lim):
+                        if c != a:
+                            if val[c] < val[b] + 2*val[a]:
+                                z += freq[val[c]]
+                            else:
+                                break
+                    x += y*z
+            p = (f*(f-1))//2
+            ans += x*p
+        
+        
+        # if 2 <= f:
+        #     x = 0
+        #     for b in range(lim-1):
+        #         if b != a:
+        #             print(val[a],val[b])
+        #             y = freq[val[b]]
+        #             z = 0 
+        #             for c in range(b+1,lim):
+        #                 if c != a:
+        #                     if val[b] + 2*val[a] <= val[c]:
+        #                         break
+                    
+        #             if val[c] < val[b] + 2*val[a]:
+        #                 if a == c:
+        #                     if a-1 == b:
+        #                         z = 0
+        #                     else:
+        #                         z = suma[val[a-1]] - suma[val[b]]
+        #                 elif a < c and a < b:
+        #                     z = suma[val[c]] - suma[val[b]]
+        #                 elif a < c:
+        #                     z = suma[val[c]] - f - suma[val[b]] 
+        #                 else:
+        #                     z = suma[val[c]] - suma[val[b]]
+        #             else:
+        #                 z = 0
+        #             print('-',y,z)
+        #             x += y*z
+        #     p = (f*(f-1))//2
+        #     ans += x*p
 
-    ans = ['']*ln
-    ans[1], ans[-2] = W[0], W[-1]
-    for x in range(0,ln+1):
-        if x%2 == 1 and 1 < x:
-            ans[x] = chr((26 + (ord(W[x-1])-65) - (ord(ans[x-2])-65))%26 + 65)
-        elif x%2 == 0 and 2 < x:
-            y = ln-x
-            ans[y] = chr((26 + (ord(W[y+1])-65) - (ord(ans[y+2])-65))%26 + 65)
-            
-    print('Case #{}: {}'.format(t+1,''.join(ans)))
+            # x = 0
+            # for b in range(lim-1):
+            #     if b != a:
+            #         y = freq[val[b]]
+            #         l, r = b+1, lim-1
+            #         while l < r:
+            #             if l+1 == r:
+            #                 k = r
+            #             else:
+            #                 k = (l+r)//2
+                            
+            #             if val[k] < val[b] + 2*val[a]:
+            #                 l = k
+            #             else:
+            #                 r = k-1
+                    
+            #         if val[l] < val[b] + 2*val[a]:
+            #             if a == l:
+            #                 if a-1 == b:
+            #                     z = 0
+            #                 else:
+            #                     z = suma[val[a-1]] - suma[val[b]]
+            #             elif a < l and a < b:
+            #                 z = suma[val[l]] - suma[val[b]]
+            #             elif a < l:
+            #                 z = suma[val[l]] - f - suma[val[b]] 
+            #             else:
+            #                 z = suma[val[l]] - suma[val[b]]
+            #         else:
+            #             z = 0
+
+            #         x += y*z
+
+            # p = (f*(f-1))//2
+            # ans += x*p
+
+    print('Case #{}: {}'.format(t+1,ans))
