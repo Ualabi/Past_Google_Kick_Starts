5b4a2dd7132db96757aa2f63eab0aeb0c051b160
Joseandres <jhinojoza@outlook.com>
2021-04-24 20:16:21 -0500

    curr version

--- pruebas.py
+++ pruebas.py
@@ -1,18 +1,36 @@
-import time
-start = time.time()
+from heapq import heappop, heappush
 
-N = 7
-primes = [2,3]
-for i in range(7,10**N):
-    flag = True
-    for prime in primes:
-        if i%prime == 0:
-            flag = False
-            break
-        elif prime*prime > i:
-            break
-    if flag:
-        primes.append(i)
-
-end = time.time()
-print(N, len(primes), end - start)
+T = int(input())
+for t in range(T):
+    N = int(input())
+    
+    arr = []
+    time = 0
+    for n in range(N):
+        a, b = map(int, input().split())
+        arr.append([a,b])
+        time += a
+    
+    heap = []
+    curr, maxi = 0, 0
+    for n in range(N):
+        a,b = arr[n]
+        
+        if a+b > curr+time:
+            maxi = max(maxi, 2*curr+time)
+            
+            time -= a
+            while heap and curr+time < -heap[0][0]:
+                c,d = heappop(heap)
+                curr -= d
+        else:
+            time -= a
+            curr += a
+            heappush(heap,(-a-b,a))
+        print(heap)
+    if heap:
+        print("Case #{}: INDEFINITELY".format(t+1))
+    else:
+        print("Case #{}: {}".format(t+1, maxi))
+    
+    
